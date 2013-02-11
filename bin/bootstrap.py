b415e296cd8f42d961954bb8a49564a16a85d0e0
Jesse the Game <jtg@li499-237.members.linode.com>
2013-02-11 19:48:17 +0000

    Update bootstrap script

--- bin/bootstrap.py
+++ bin/bootstrap.py
@@ -4,11 +4,8 @@ import os
 import sys
 import subprocess
 
-#TODO: make a setenv manage command
-
 ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 NAME = os.path.split(ROOT)[1]
-LOCAL = os.path.join(ROOT, 'local')
 
 def try_mkdir(path):
     try:
@@ -18,15 +15,14 @@ def try_mkdir(path):
 
 def main():
     try:
-        env = sys.argv[1]
+        LOCAL = sys.argv[1]
     except IndexError:
-        exit("Usage: python bootstrap.py <name>")
+        LOCAL = os.path.join(ROOT, 'local')
 
     print 'cd %s' % ROOT
     os.chdir(ROOT)
     print 'mkdir local'
     try_mkdir(LOCAL)
-
     print 'cd local'
     os.chdir(LOCAL)
     print 'mkdir log'
@@ -39,28 +35,19 @@ def main():
     try_mkdir('celery')
 
     # Create the virtualenv with virtualenvwrapper
-    envname = '%s@%s' % (env, NAME)
-    envdir = os.path.join(LOCAL, envname)
+    ENV = os.path.join(LOCAL, 'venv')
 
-    print 'virtualenv', envdir
-    subprocess.call(['virtualenv', envdir])
+    print 'virtualenv', ENV
+    subprocess.call(['virtualenv', ENV])
 
-    try:
-        print 'ln -s %s venv' % os.path.join(envdir)
-        os.symlink(envdir, 'venv')
-    except OSError:
-        pass
-
-
-    with open('ENV', 'w') as f:
-        f.write(env)
-
-    subprocess.call([os.path.join(envdir, 'bin', 'activate')])
+    activate = os.path.join(ENV, 'bin', 'activate')
+    print activate
+    subprocess.call([activate])
 
     print 'cd ..'
     os.chdir(ROOT)
 
-    print 'pip install -r requirements.pip', envdir
+    print 'pip install -r requirements.pip'
     subprocess.call(['pip', 'install', '-r', os.path.join(ROOT.encode(), 'requirements.pip')])
 
 
